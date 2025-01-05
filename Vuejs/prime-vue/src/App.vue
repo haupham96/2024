915cd1fc5926b983de8cd6da6568227fb90ae142
marco.pham <marco.pham@bowkergarment.com.vn>
2025-01-05 12:28:20 +0700

    hihi2

---
+++ Vuejs/prime-vue/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import InputText from 'primevue/inputtext'
+import Button from 'primevue/button'
+import DatePicker from 'primevue/datepicker'
+</script>
+
+<template>
+  <InputText type="text" v-model="value" />
+  <Button label="Submit" />
+  <DatePicker v-model="date" showIcon showTime iconDisplay="input" />
+</template>
+
+<style scoped></style>
